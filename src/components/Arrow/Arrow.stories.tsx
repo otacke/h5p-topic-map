f15949c5f75ecdec5b8a12f48e659edfe77d89d5
Hans Ivar Botnen <hansibotnen@gmail.com>
2021-09-21 16:50:26 +0200

    run prettier and eslint
    fix eslint problems
    
    run prettier again

--- src/components/Arrow/Arrow.stories.tsx
+++ src/components/Arrow/Arrow.stories.tsx
@@ -8,78 +8,78 @@ const blue = "#114df3";
 const red = "#df0c0c";
 
 export default {
-    title: "Arrow",
-    component: Arrow,
-    argTypes: {
-        color: {
-            default: "#000000",
-            control: 'color'
-        },
+  title: "Arrow",
+  component: Arrow,
+  argTypes: {
+    color: {
+      default: "#000000",
+      control: "color",
     },
+  },
 } as ComponentMeta<typeof Arrow>;
 
 const defaultArgs: ArrowProps = {
-    start: {
-        x: 0,
-        y: 0,
-    },
-    end: {
-        x: 0,
-        y: 0,
-    },
-    color: blue,
+  start: {
+    x: 0,
+    y: 0,
+  },
+  end: {
+    x: 0,
+    y: 0,
+  },
+  color: blue,
 };
 
-const Template: ComponentStory<typeof Arrow> = args => <Arrow {...args} />
+const Template: ComponentStory<typeof Arrow> = args => <Arrow {...args} />;
 
 export const RightPointingArrow = Template.bind({});
 RightPointingArrow.args = {
-    start: {
-        x: 0,
-        y: 0,
-    },
-    end: {
-        x: 100,
-        y: 0,
-    },
-    color: blue,
+  start: {
+    x: 0,
+    y: 0,
+  },
+  end: {
+    x: 100,
+    y: 0,
+  },
+  color: blue,
 };
 
 export const LeftPointingArrow = Template.bind({});
 LeftPointingArrow.args = {
-    start: {
-        x: 100,
-        y: 0,
-    },
-    end: {
-        x: 0,
-        y: 0,
-    },
-    color: red,
+  start: {
+    x: 100,
+    y: 0,
+  },
+  end: {
+    x: 0,
+    y: 0,
+  },
+  color: red,
 };
 
 export const UpPointingArrow = Template.bind({});
 UpPointingArrow.args = {
-    start: {
-        x: 0,
-        y: 100,
-    },
-    end: {
-        x: 0,
-        y: 0,
-    },
-    color: red,
+  start: {
+    x: 0,
+    y: 100,
+  },
+  end: {
+    x: 0,
+    y: 0,
+  },
+  color: red,
 };
 
 export const DownPointingArrow = Template.bind({});
 DownPointingArrow.args = {
-    start: {
-        x: 0,
-        y: 0,
-    },
-    end: {
-        x: 0,
-        y: 100,
-    },
-    color: red,
+  start: {
+    x: 0,
+    y: 0,
+  },
+  end: {
+    x: 0,
+    y: 100,
+  },
+  color: red,
 };
--- src/components/Arrow/Arrow.tsx
+++ src/components/Arrow/Arrow.tsx
@@ -3,57 +3,53 @@ import { Position } from "../../types/Position";
 import styles from "./Arrow.module.scss";
 
 export type ArrowProps = {
-    start: Position;
-    end: Position;
-    color: string;
-}
+  start: Position;
+  end: Position;
+  color: string;
+};
 
-export const Arrow: React.FC<ArrowProps> = ({
-    start,
-    end,
-    color,
-}) => {
+export const Arrow: React.FC<ArrowProps> = ({ start, end, color }) => {
+  const arrowHead = (
+    <svg
+      className={styles.head}
+      viewBox="0 0 20 40"
+      preserveAspectRatio="xMaxYMid"
+    >
+      <polygon points="0,0 0,40 20,20" fill={color} />
+    </svg>
+  );
 
-    const arrowHead = (
-        <svg className={styles.head} viewBox="0 0 20 40" preserveAspectRatio="xMaxYMid">
-            <polygon points="0,0 0,40 20,20" fill={color}>
-            </polygon>
-        </svg>);
+  const arrowBody = (
+    <svg className={styles.body} viewBox="0 0 1 40" preserveAspectRatio="none">
+      <rect x="0" y="15" width="1" height="10" fill={color} />
+    </svg>
+  );
 
-    const arrowBody = (
-        <svg className={styles.body} viewBox="0 0 1 40" preserveAspectRatio="none">
-            <rect x="0" y="15" width="1" height="10" fill={color} />
-        </svg>);
+  // find angle and direction of arrow
+  let angle = Math.atan2(start.y - end.y, end.x - start.x) * (180 / Math.PI);
+  if (angle < 0) angle = 360 + angle;
 
-    // find angle and direction of arrow
-    let angle = Math.atan2(start.y - end.y, end.x - start.x) * (180 / Math.PI);
-    if (angle < 0) angle = 360 + angle;
-    
-    let pointsUp = (angle > 45 && angle < 135);
-    let pointsDown = (angle > 225 && angle < 315);
-    let pointsLeft = (angle >= 135 && angle <= 225);
+  const pointsUp = angle > 45 && angle < 135;
+  const pointsDown = angle > 225 && angle < 315;
+  const pointsLeft = angle >= 135 && angle <= 225;
 
-    let classNames = styles.arrow + ' ';
-    let length;
+  let classNames = `${styles.arrow} `;
+  let length;
 
-    if (pointsUp || pointsDown) {
-        length = { width: Math.abs(end.y - start.y) };
-        if (pointsUp)
-            classNames += styles.pointUp;
-        else
-            classNames += styles.pointDown;
-    }
-    else {
-        length = { width: Math.abs(end.x - start.x) };
-        if (pointsLeft)
-            classNames += styles.pointLeft
-    }
+  if (pointsUp || pointsDown) {
+    length = { width: Math.abs(end.y - start.y) };
+    if (pointsUp) classNames += styles.pointUp;
+    else classNames += styles.pointDown;
+  } else {
+    length = { width: Math.abs(end.x - start.x) };
+    if (pointsLeft) classNames += styles.pointLeft;
+  }
 
-    let arrow = (
-        <div className={classNames} style={length}>
-            {arrowBody}
-            {arrowHead}
-        </div>
-    );
-    return arrow;
+  const arrow = (
+    <div className={classNames} style={length}>
+      {arrowBody}
+      {arrowHead}
+    </div>
+  );
+  return arrow;
 };
