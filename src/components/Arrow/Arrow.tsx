995d4a5ac9b0ed81c85a58449b994f5f87c220aa
Hans Ivar Botnen <hansibotnen@gmail.com>
2021-09-21 16:30:10 +0200

    Creating arrow finds correct direction and length

--- src/components/Arrow/Arrow.tsx
+++ src/components/Arrow/Arrow.tsx
@@ -1,4 +1,3 @@
-import { jSXElement, JSX_TYPES } from "@babel/types";
 import * as React from "react";
 import { Position } from "../../types/Position";
 import styles from "./Arrow.module.scss";
@@ -9,60 +8,52 @@ export type ArrowProps = {
     color: string;
 }
 
-export const Arrow: React.FC<ArrowProps>= ({
+export const Arrow: React.FC<ArrowProps> = ({
     start,
     end,
     color,
 }) => {
-    
-    
-    let width = Math.abs(start.x) + Math.abs(end.x);
-    let height = Math.abs(start.y) + Math.abs(end.y);
 
-    //length and angle of arrow
-    let length =  Math.sqrt(Math.pow((end.x-start.x),2) + Math.pow((end.y-start.y),2));
-    let angle = Math.atan2(end.y-start.y, end.x-start.x) * 180 / Math.PI;
+    const arrowHead = (
+        <svg className={styles.head} viewBox="0 0 20 40" preserveAspectRatio="xMaxYMid">
+            <polygon points="0,0 0,40 20,20" fill={color}>
+            </polygon>
+        </svg>);
 
-    // coordinates where arrow body should end and arrow head should begin
-    let neckX = start.x + (length-20)*Math.cos(angle);
-    let neckY = start.y + (length-20)*Math.sin(angle);
-    
-    
-    const arrowHeadRight = (
-    <svg className={styles.head} viewBox="0 0 20 40" preserveAspectRatio="xMaxYMid">
-    <polygon points="0,0 0,40 20,20" fill={color}>
-    </polygon>
-    </svg>);
+    const arrowBody = (
+        <svg className={styles.body} viewBox="0 0 1 40" preserveAspectRatio="none">
+            <rect x="0" y="15" width="1" height="10" fill={color} />
+        </svg>);
 
-    const arrowBodyHorizontal = (
-    <svg className={styles.body} viewBox="0 0 1 40" preserveAspectRatio="none">
-    <rect x="0" y="15" width="1" height="10" fill={color}/>
-    </svg>);
+    // find angle and direction of arrow
+    let angle = Math.atan2(start.y - end.y, end.x - start.x) * (180 / Math.PI);
+    if (angle < 0) angle = 360 + angle;
+    
+    let pointsUp = (angle > 45 && angle < 135);
+    let pointsDown = (angle > 225 && angle < 315);
+    let pointsLeft = (angle >= 135 && angle <= 225);
 
     let classNames = styles.arrow + ' ';
+    let length;
 
-    if(start.x > end.x){
-        classNames+=styles.pointLeft;
+    if (pointsUp || pointsDown) {
+        length = { width: Math.abs(end.y - start.y) };
+        if (pointsUp)
+            classNames += styles.pointUp;
+        else
+            classNames += styles.pointDown;
     }
-    if(start.y > end.y){
-        classNames+=styles.pointDown;
-    }
-    else if(start.y < end.y){
-        classNames+=styles.pointUp;
+    else {
+        length = { width: Math.abs(end.x - start.x) };
+        if (pointsLeft)
+            classNames += styles.pointLeft
     }
 
-    let arrow = ( 
-    <div className={classNames}>
-        {arrowBodyHorizontal}
-        {arrowHeadRight}
-    </div>
+    let arrow = (
+        <div className={classNames} style={length}>
+            {arrowBody}
+            {arrowHead}
+        </div>
     );
-
-    //Right pointing arrow
-    
-    // todo: make different types of arrows
-    
-
-
-    return arrow;   
+    return arrow;
 };
