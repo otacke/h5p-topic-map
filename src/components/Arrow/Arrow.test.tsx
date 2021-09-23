b408ef1b37c3bbbdf0d4c15515c859e016751a67
Hans Ivar Botnen <43134875+hansbotnen@users.noreply.github.com>
2021-09-23 09:51:33 +0200

    add additional tests
    Co-authored-by: Sindre Bøyum <boyum@users.noreply.github.com>

--- src/components/Arrow/Arrow.test.tsx
+++ src/components/Arrow/Arrow.test.tsx
@@ -14,5 +14,34 @@ describe(Arrow.name, () => {
     ).container;
 
     expect(arrow.querySelector("div")).toBeTruthy();
+    expect(arrow.querySelectorAll("svg").length).toBe(2);
+  });
+  
+  it("should add two arrow heads if the arrow is bidirectional", () => {
+    const arrow = render(
+      <Arrow
+        start={{ x: 0, y: 0 }}
+        end={{ x: 100, y: 0 }}
+        color="#000000"
+        type={ArrowType.BiDirectional}
+      />,
+    ).container;
+
+    expect(arrow.querySelector("div")).toBeTruthy();
+    expect(arrow.querySelectorAll("svg").length).toBe(3);
+  });
+  
+  it("should render", () => {
+    const arrow = render(
+      <Arrow
+        start={{ x: 0, y: 0 }}
+        end={{ x: 100, y: 0 }}
+        color="#000000"
+        type={ArrowType.NonDirectional}
+      />,
+    ).container;
+
+    expect(arrow.querySelector("div")).toBeTruthy();
+    expect(arrow.querySelectorAll("svg").length).toBe(1);
   });
 });
