5405a52c78c7ddee6a4839d454b5f8a371d3562e
Yijun <yijun.pan.stautland@tietoevry.com>
2021-09-27 15:02:36 +0200

    Add empty button to icon
    Remove console log
    
    Run prettier

--- src/components/Arrow/Arrow.tsx
+++ src/components/Arrow/Arrow.tsx
@@ -27,13 +27,14 @@ export type ArrowProps = {
 const makeMirroredHead = (arrowColor: string) => {
   return (
     <svg
-      className= {`${styles.head} ${styles.mirrorX}`}
+      className={`${styles.head} ${styles.mirrorX}`}
       viewBox="0 0 20 40"
       preserveAspectRatio="xMaxYMid"
     >
       <polygon points="0,0 0,40 20,20" fill={arrowColor} />
-    </svg>);
-}
+    </svg>
+  );
+};
 
 const makeHead = (arrowColor: string) => {
   return (
@@ -43,8 +44,9 @@ const makeHead = (arrowColor: string) => {
       preserveAspectRatio="xMaxYMid"
     >
       <polygon points="0,0 0,40 20,20" fill={arrowColor} />
-    </svg>);
-}
+    </svg>
+  );
+};
 
 const makeBody = (arrowColor: string) => {
   return (
@@ -52,53 +54,85 @@ const makeBody = (arrowColor: string) => {
       <rect x="0" y="15" width="1" height="10" fill={arrowColor} />
     </svg>
   );
-}
+};
 
 const renderIcons = (hasNotes: boolean, iconColor: string) => {
-  const edit = <path fill={iconColor} d="M20.71,7.04C21.1,6.65 21.1,6 20.71,5.63L18.37,3.29C18,2.9 17.35,2.9 16.96,3.29L15.12,5.12L18.87,8.87M3,17.25V21H6.75L17.81,9.93L14.06,6.18L3,17.25Z" />
-  const notes = <path fill={iconColor} d="M20,20H4A2,2 0 0,1 2,18V6A2,2 0 0,1 4,4H20A2,2 0 0,1 22,6V18A2,2 0 0,1 20,20M4,6V18H20V6H4M6,9H18V11H6V9M6,13H16V15H6V13Z" />
+  const edit = (
+    <path
+      fill={iconColor}
+      d="M20.71,7.04C21.1,6.65 21.1,6 20.71,5.63L18.37,3.29C18,2.9 17.35,2.9 16.96,3.29L15.12,5.12L18.87,8.87M3,17.25V21H6.75L17.81,9.93L14.06,6.18L3,17.25Z"
+    />
+  );
+  const notes = (
+    <path
+      fill={iconColor}
+      d="M20,20H4A2,2 0 0,1 2,18V6A2,2 0 0,1 4,4H20A2,2 0 0,1 22,6V18A2,2 0 0,1 20,20M4,6V18H20V6H4M6,9H18V11H6V9M6,13H16V15H6V13Z"
+    />
+  );
   return hasNotes ? notes : edit;
-}
+};
 
-const makeCircle = (arrowColor: string, circleColor: string, iconColor: string, type: ArrowType, direction: ArrowDirection) => {
+const makeButton = (
+  arrowColor: string,
+  circleColor: string,
+  iconColor: string,
+  type: ArrowType,
+  direction: ArrowDirection,
+) => {
   let classNames = `${styles.button}`;
 
-  if(type !== ArrowType.Directional)
+  if (type !== ArrowType.Directional)
     classNames += ` ${styles.buttonNonDirectional}`;
-  else
-    classNames += ` ${styles.buttonDirectional}`;
+  else classNames += ` ${styles.buttonDirectional}`;
 
-  switch(direction){
-    case ArrowDirection.Up: classNames += ` ${styles.buttonUp}`; break;
-    case ArrowDirection.Down: classNames += ` ${styles.buttonDown}`; break;
-    case ArrowDirection.Left: classNames += ` ${styles.buttonLeft}`; break;
-    case ArrowDirection.Right: classNames += ` ${styles.buttonRight}`; break;
+  switch (direction) {
+    case ArrowDirection.Up:
+      classNames += ` ${styles.buttonUp}`;
+      break;
+    case ArrowDirection.Down:
+      classNames += ` ${styles.buttonDown}`;
+      break;
+    case ArrowDirection.Left:
+      classNames += ` ${styles.buttonLeft}`;
+      break;
+    case ArrowDirection.Right:
+      classNames += ` ${styles.buttonRight}`;
+      break;
   }
 
   return (
-  <svg
-    className={classNames}
-    viewBox="0 0 12 12"
-    preserveAspectRatio="xMaxYMid"
-  >
-    <circle
-      cx="6"
-      cy="6"
-      r="4px"
-      stroke={circleColor}
-      fill={arrowColor}
-      strokeWidth="0.7"
-    />
+    <svg
+      className={classNames}
+      onClick={clickArrow}
+      viewBox="0 0 12 12"
+      preserveAspectRatio="xMaxYMid"
+    >
+      <circle
+        cx="6"
+        cy="6"
+        r="4px"
+        stroke={circleColor}
+        fill={arrowColor}
+        strokeWidth="0.7"
+      />
 
-    <svg viewBox="-12 -12 48 48">
-      {renderIcons(true, iconColor)}
+      <svg viewBox="-12 -12 48 48">{renderIcons(true, iconColor)}</svg>
     </svg>
-  </svg>
   );
-}
+};
 
-export const Arrow: React.FC<ArrowProps> = ({ start, end, arrowColor, circleColor, iconColor, type }) => {
+const clickArrow = () => {
+  // TODO
+};
 
+export const Arrow: React.FC<ArrowProps> = ({
+  start,
+  end,
+  arrowColor,
+  circleColor,
+  iconColor,
+  type,
+}) => {
   // find angle and direction of arrow
   let angle = Math.atan2(start.y - end.y, end.x - start.x) * (180 / Math.PI);
   if (angle < 0) angle = 360 + angle;
@@ -113,21 +147,19 @@ export const Arrow: React.FC<ArrowProps> = ({ start, end, arrowColor, circleColo
 
   if (pointsUp || pointsDown) {
     length = { width: Math.abs(end.y - start.y) };
-    if (pointsUp){ 
+    if (pointsUp) {
       classNames += styles.pointUp;
       direction = ArrowDirection.Up;
-    }
-    else {
+    } else {
       classNames += styles.pointDown;
       direction = ArrowDirection.Down;
     }
   } else {
     length = { width: Math.abs(end.x - start.x) };
-    if (pointsLeft){
+    if (pointsLeft) {
       classNames += styles.pointLeft;
       direction = ArrowDirection.Left;
-    } 
-    else {
+    } else {
       classNames += styles.pointRight;
       direction = ArrowDirection.Right;
     }
@@ -139,7 +171,7 @@ export const Arrow: React.FC<ArrowProps> = ({ start, end, arrowColor, circleColo
       arrow = (
         <div className={classNames} style={length}>
           {makeBody(arrowColor)}
-          {makeCircle(arrowColor, circleColor, iconColor, type, direction)}
+          {makeButton(arrowColor, circleColor, iconColor, type, direction)}
         </div>
       );
       break;
@@ -149,7 +181,7 @@ export const Arrow: React.FC<ArrowProps> = ({ start, end, arrowColor, circleColo
           {makeMirroredHead(arrowColor)}
           {makeBody(arrowColor)}
           {makeHead(arrowColor)}
-          {makeCircle(arrowColor, circleColor, iconColor, type, direction)}
+          {makeButton(arrowColor, circleColor, iconColor, type, direction)}
         </div>
       );
       break;
@@ -158,7 +190,7 @@ export const Arrow: React.FC<ArrowProps> = ({ start, end, arrowColor, circleColo
         <div className={classNames} style={length}>
           {makeBody(arrowColor)}
           {makeHead(arrowColor)}
-          {makeCircle(arrowColor, circleColor, iconColor, type, direction)}
+          {makeButton(arrowColor, circleColor, iconColor, type, direction)}
         </div>
       );
   }
