62f2ba299e21d1f313bf6a4ff566a3689ccb1730
Hans Ivar Botnen <hansibotnen@gmail.com>
2021-10-11 16:19:38 +0200

    added dialogwindow root, overlay and trigger

--- src/components/Dialog-Window/DialogWindow.tsx
+++ src/components/Dialog-Window/DialogWindow.tsx
@@ -4,11 +4,29 @@ import * as Dialog from "@radix-ui/react-dialog";
 import styles from "./DialogWindow.module.scss";
 
 export type DialogWindowProps = {
+  title: string;
   notes: string;
 };
 
-export const DialogWindow: React.FC<DialogWindowProps> = (
-  props,
-): JSX.Element => {
-  return <div>Hello Modal</div>;
+const DialogRoot = ({ children, ...props }: any): JSX.Element => {
+  return (
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    <Dialog.Root {...props}>
+      <Dialog.Overlay className={styles.overlay} />
+      {children}
+    </Dialog.Root>
+  );
+};
+
+export const DialogWindow: React.FC<DialogWindowProps> = ({
+  title,
+  notes,
+}): JSX.Element => {
+  return (
+    <DialogRoot>
+      <Dialog.Trigger asChild>
+        <button type="button">Open Dialog</button>
+      </Dialog.Trigger>
+    </DialogRoot>
+  );
 };
