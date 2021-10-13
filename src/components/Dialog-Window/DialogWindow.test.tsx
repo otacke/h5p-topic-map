1128eb107a45e56ef2982caed85f6f99999a385c
Hans Ivar Botnen <hansibotnen@gmail.com>
2021-10-13 13:13:20 +0200

    fixed tests

--- src/components/Dialog-Window/DialogWindow.test.tsx
+++ src/components/Dialog-Window/DialogWindow.test.tsx
@@ -2,10 +2,17 @@ import * as React from "react";
 import { getByText, render } from "@testing-library/react";
 import { DialogWindow } from "./DialogWindow";
 
+const onOpenChange = console.info;
+
 describe(DialogWindow.name, () => {
   it("should render", () => {
     const dialogWindow = render(
-      <DialogWindow notes="test" title="dialog-window-test" />,
+      <DialogWindow
+        notes="test"
+        title="dialog-window-test"
+        open={false}
+        onOpenChange={onOpenChange}
+      />,
     ).container;
 
     setTimeout(() => {
@@ -15,7 +22,12 @@ describe(DialogWindow.name, () => {
 
   it("should render the correct title", () => {
     const dialogWindow = render(
-      <DialogWindow notes="test" title="dialog-window-test" />,
+      <DialogWindow
+        notes="test"
+        title="dialog-window-test"
+        open={false}
+        onOpenChange={onOpenChange}
+      />,
     ).container;
 
     setTimeout(() => {
