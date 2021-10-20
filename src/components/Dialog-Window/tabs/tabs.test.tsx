c040e50b70f0504f1a69b47b89966c4abbad8388
Yijun <yijun.pan.stautland@tietoevry.com>
2021-10-20 22:28:05 +0200

    Add a simple render test

--- src/components/Dialog-Window/tabs/tabs.test.tsx
+++ src/components/Dialog-Window/tabs/tabs.test.tsx
@@ -1,3 +1,14 @@
-export type DialogTabProps = {
-  titles: string[];
-};
+import { render } from "@testing-library/react";
+import * as React from "react";
+import { Tabs } from "./tabs";
+
+
+describe(Tabs.name, () => {
+  it("should have rendered.", () => {
+  const tabs = render(
+    <Tabs tabContents={[{ title: "Text", content: <h1>Content for text goes here!</h1> }]} />
+  ).container;
+
+  expect(tabs.querySelector("div")).toBeTruthy();
+  });
+})
--- src/components/Dialog-Window/tabs/tabs.tsx
+++ src/components/Dialog-Window/tabs/tabs.tsx
@@ -1,29 +1,24 @@
+/* eslint-disable react/no-array-index-key */
 import * as React from "react";
 import { Root, List, Trigger, Content } from "@radix-ui/react-tabs";
-import { styled } from "@stitches/react";
 import styles from "./tabs.module.scss";
 
 export type TabProps = {
   tabContents: { title: string; content: JSX.Element }[];
 };
 
-export const Tabs: React.FC<TabProps> = ({ tabContents }): JSX.Element => {
+export const Tabs: React.FC<TabProps> = ({ tabContents }) => {
   return (
-    <Root
-      className={styles.tabs}
-      defaultValue={tabContents[0].title}
-      orientation="vertical"
-    >
+    <Root className={styles.tabs} defaultValue={tabContents[0].title} orientation="vertical">
       <List className={styles.list} aria-label="tabs example">
-        {tabContents.map(el => (
-          <Trigger className={styles.trigger} value={el.title}>
-            {" "}
-            {el.title}{" "}
+        {tabContents.map((el, i) => (
+          <Trigger key={i} className={styles.trigger} value={el.title}>
+            {el.title}
           </Trigger>
         ))}
       </List>
-      {tabContents.map(el => (
-        <Content value={el.title}> {el.content} </Content>
+      {tabContents.map((el, i) => (
+        <Content key={i} value={el.title}> {el.content} </Content>
       ))}
     </Root>
   );
