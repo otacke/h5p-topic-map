501cb9c70a2131ea974233fc6021199880b5e194
Yijun <yijun.pan.stautland@tietoevry.com>
2021-10-25 15:30:46 +0200

    Add basic css and list and input field

--- src/components/Dialog-Window/Resources/DialogResources.tsx
+++ src/components/Dialog-Window/Resources/DialogResources.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import styles from "./DialogResources.module.scss";
 
 export type DialogResourceProps = {
   links: string[];
@@ -8,7 +9,16 @@ export const DialogResources: React.FC<DialogResourceProps> = ({ links }) => {
   return (
     <div>
       <p> Relevante lenker: </p>
+        <ul>
+          <li className={styles.li}><a href="www.google.com">www.google.com</a></li>
+          <li className={styles.li}><a href="www.yahoo.com">www.yahoo.com</a></li>
+        </ul>
       <p> Dine lenker: </p>
+
+      <div>
+        <input className={styles.input} placeholder="www.nyresurss.no" />
+        <input className={styles.inputButton} type="button" value="Legg til" />
+      </div>
     </div>
   );
 };
