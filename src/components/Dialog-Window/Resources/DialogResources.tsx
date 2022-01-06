9ec40d5b8d39645e0b22947a6ceb6cb50a7ef6bd
Yijun <yijun.pan.stautland@tietoevry.com>
2022-01-06 06:29:30 +0100

    Update CSS and add lists as props

--- src/components/Dialog-Window/Resources/DialogResources.tsx
+++ src/components/Dialog-Window/Resources/DialogResources.tsx
@@ -2,18 +2,32 @@ import * as React from "react";
 import styles from "./DialogResources.module.scss";
 
 export type DialogResourceProps = {
-  links: string[];
+  relevantLinks: string[];
+  customLinks: string[];
 };
 
-export const DialogResources: React.FC<DialogResourceProps> = ({ links }) => {
+export const DialogResources: React.FC<DialogResourceProps> = ({
+  relevantLinks,
+  customLinks,
+}) => {
+  const relevantItems = relevantLinks.map((link: string) => (
+    <li key={link} className={styles.li}>
+      <a href={link}>{link}</a>
+    </li>
+  ));
+
+  const customItems = customLinks.map((link: string) => (
+    <li key={link} className={styles.li}>
+      <a href={link}>{link}</a>
+    </li>
+  ));
+
   return (
     <div>
       <p> Relevante lenker: </p>
-        <ul>
-          <li className={styles.li}><a href="www.google.com">www.google.com</a></li>
-          <li className={styles.li}><a href="www.yahoo.com">www.yahoo.com</a></li>
-        </ul>
+      <ul>{relevantItems}</ul>
       <p> Dine lenker: </p>
+      <ul>{customItems}</ul>
 
       <div>
         <input className={styles.input} placeholder="www.nyresurss.no" />
