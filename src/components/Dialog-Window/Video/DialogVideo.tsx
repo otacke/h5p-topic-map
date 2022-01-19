7fb4708dfc8db0c21a2482cb86a684ccad0fbec3
Yijun <yijun.pan.stautland@tietoevry.com>
2022-01-19 15:17:45 +0100

    Add description and styling

--- src/components/Dialog-Window/Video/DialogVideo.tsx
+++ src/components/Dialog-Window/Video/DialogVideo.tsx
@@ -1,14 +1,19 @@
+/* eslint-disable jsx-a11y/media-has-caption */
 import * as React from "react";
+import styles from "./DialogVideo.module.scss";
 
 export type DialogVideoProps = {
   video: { path: string };
+  description: string;
 };
 
-export const DialogVideo: React.FC<DialogVideoProps> = ({ video }) => {
+export const DialogVideo: React.FC<DialogVideoProps> = ({ video, description}) => {
   return (
-    // eslint-disable-next-line jsx-a11y/media-has-caption
-    <video controls>
-      <source src={video?.path} />
-    </video>
+    <>
+      <video controls>
+        <source src={video?.path} />
+      </video>
+      <p className={styles.description}>{description}</p>
+    </>
   );
 };
