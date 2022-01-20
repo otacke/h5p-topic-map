f8d34b2941e5da5e2e2553e2e3ee6d731f3b7d22
Yijun <yijun.pan.stautland@tietoevry.com>
2022-01-20 08:15:37 +0100

    Add render test

---
+++ src/components/Dialog-Window/Text/DialogText.test.tsx
@@ -0,0 +1,13 @@
+import { render } from "@testing-library/react";
+import * as React from "react";
+import { DialogText } from "./DialogText";
+
+describe(DialogText.name, () => {
+    it("Should have rendered", () => {
+        const text = render(
+            <DialogText text={<div/>}/>
+        ).container;
+
+        expect(text.querySelector("div"));
+    });
+})
