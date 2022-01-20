e3b381c35e64ee6f1cbcad0c6ad527ffc32dd601
Yijun <yijun.pan.stautland@tietoevry.com>
2022-01-20 08:20:42 +0100

    Run prettier

--- src/components/Dialog-Window/Text/DialogText.test.tsx
+++ src/components/Dialog-Window/Text/DialogText.test.tsx
@@ -3,11 +3,9 @@ import * as React from "react";
 import { DialogText } from "./DialogText";
 
 describe(DialogText.name, () => {
-    it("Should have rendered", () => {
-        const text = render(
-            <DialogText text={<div/>}/>
-        ).container;
+  it("Should have rendered", () => {
+    const text = render(<DialogText text={<div />} />).container;
 
-        expect(text.querySelector("div"));
-    });
-})
+    expect(text.querySelector("div"));
+  });
+});
