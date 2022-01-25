f3c78834e6a2341b24e58334d6200906d437b660
Sindre Bøyum <sindre.boyum@gmail.com>
2022-01-25 14:13:26 +0100

    add copyright fields on H5P Image type

--- src/types/Image.ts
+++ src/types/Image.ts
@@ -1,4 +1,10 @@
+import { Copyright } from "./H5P/Copyright";
+
 export type Image = {
   path: string;
-  alt?: string | undefined;
+  alt?: string;
+  copyright?: Copyright;
+  height?: number;
+  width?: number;
+  mime?: string;
 };
