00e6611c22f44921ffc6c11238a0a9ba0b8a2356
Sindre Bøyum <sindre.boyum@gmail.com>
2022-01-25 16:35:34 +0100

    add copyright to image text

--- src/components/Dialog-Window/Text/DialogText.stories.tsx
+++ src/components/Dialog-Window/Text/DialogText.stories.tsx
@@ -15,27 +15,25 @@ const Template: ComponentStory<typeof DialogText> = args => (
 export const SimpleText = Template.bind({});
 
 SimpleText.args = {
-  text: (
-    <>
-      <b>
-        Den franske revolusjon var en periode med store sosiale og politiske
-        omveltningene i Frankrike i perioden 1789-1799. Året 1789 markerer det
-        første viktige vendepunktet under revolusjonen. 14. juli dette året brøt
-        det ut masseopprør i Paris og fengselet Bastillen ble stormet.
-      </b>
-      <br />
-      <p>
-        Perioden 1787-1789 kalles gjerne førrevolusjonen, men langsiktige
-        årsaker til revolusjonen strekker seg naturligvis mye lenger tilbake i
-        tid. De viktigste politiske, sosiale og administrative omveltninger
-        skjedde i perioden 1789-1794.
-      </p>
-      <br />
-      <img
-        alt="test-img"
-        style={{ width: "100%" }}
-        src="https://upload.wikimedia.org/wikipedia/commons/4/4e/Norwegian_digital_learning_arena.jpg"
-      />
-    </>
-  ),
+  topicImage: {
+    path: "https://images.unsplash.com/photo-1557657043-23eec69b89c9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1746&q=80",
+    alt: "White seal on soil",
+    copyright: {
+      author: "Amy Asher",
+      license: "Unsplash License",
+      source: "https://unsplash.com/photos/giZJHm2m9yY",
+      title: "White seal on soil",
+      version: "1",
+      year: "2019",
+    },
+  },
+  introduction: `Den franske revolusjon var en periode med store sosiale og politiske
+  omveltningene i Frankrike i perioden 1789-1799. Året 1789 markerer det
+  første viktige vendepunktet under revolusjonen. 14. juli dette året brøt
+  det ut masseopprør i Paris og fengselet Bastillen ble stormet.`,
+
+  bodyText: `Perioden 1787-1789 kalles gjerne førrevolusjonen, men langsiktige
+  årsaker til revolusjonen strekker seg naturligvis mye lenger tilbake i
+  tid. De viktigste politiske, sosiale og administrative omveltninger
+  skjedde i perioden 1789-1794.`,
 };
