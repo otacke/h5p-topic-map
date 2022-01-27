ce7f2488aa08efd2ebecd0415e33862cce721713
Sindre Bøyum <sindre.boyum@gmail.com>
2022-01-27 13:32:58 +0100

    TranslationKeys: add `generated` warning

--- generate-semantics.ts
+++ generate-semantics.ts
@@ -10,11 +10,20 @@ async function createSemanticsJson(): Promise<void> {
 }
 
 async function createTranslationKeys(): Promise<void> {
-  const translationKeys = semantics[2].fields.map(({ name }) => name);
+  const translationKeys = semantics[2].fields
+    .map(({ name }) => name)
+    .join(`"\n  | "`);
 
-  const textContent = `export type TranslationKey = \n | "${translationKeys.join(
-    '"\n  | "',
-  )}";\n`;
+  const textContent = `// --------- ⚠️  WARNING  ⚠️ ---------
+// This file is generated from the values within \`semantics.json\`'s l10n group. 
+// Do not change it manually, but rather change \`semantics.ts\` and run
+// \`npm run generate-semantics\`.
+// -----------------------------------
+
+export type TranslationKey =
+  | "${translationKeys}";
+`;
+
   await fs.promises.writeFile(translationKeyPath, textContent);
 }
 
