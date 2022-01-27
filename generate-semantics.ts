33947e17e9946d94b33dad361aa5c5968cce4e8d
Sindre Bøyum <sindre.boyum@gmail.com>
2022-01-27 13:06:41 +0100

    get translation keys from semantics

--- generate-semantics.ts
+++ generate-semantics.ts
@@ -2,22 +2,25 @@ import * as fs from "fs";
 import { semantics } from "./src/semantics";
 
 const semanticsPath = "semantics.json";
-async function deleteSemanticsJson(): Promise<void> {
-  await fs.promises.rm(semanticsPath);
-}
+const translationKeyPath = "src/types/TranslationKey.ts";
 
 async function createSemanticsJson(): Promise<void> {
   const textContent = JSON.stringify(semantics, null, 2);
   await fs.promises.writeFile(semanticsPath, textContent);
 }
 
-async function run(): Promise<void> {
-  const semanticsJsonExists = fs.existsSync(semanticsPath);
-  if (semanticsJsonExists) {
-    await deleteSemanticsJson();
-  }
+async function createTranslationKeys(): Promise<void> {
+  const translationKeys = semantics[2].fields.map(({ name }) => name);
 
+  const textContent = `export type TranslationKey = \n | "${translationKeys.join(
+    '"\n  | "',
+  )}";\n`;
+  await fs.promises.writeFile(translationKeyPath, textContent);
+}
+
+async function run(): Promise<void> {
   await createSemanticsJson();
+  await createTranslationKeys();
 }
 
 run();
