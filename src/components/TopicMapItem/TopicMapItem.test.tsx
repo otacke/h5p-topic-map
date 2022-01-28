77fb89d9b7228a00f07b8f49a6676a287606750f
Sindre Bøyum <sindre.boyum@gmail.com>
2022-01-28 11:51:51 +0100

    fix tests and stories

--- src/components/TopicMapItem/TopicMapItem.test.tsx
+++ src/components/TopicMapItem/TopicMapItem.test.tsx
@@ -4,10 +4,29 @@ import { TopicMapItem } from "./TopicMapItem";
 
 describe(TopicMapItem.name, () => {
   it("should render", () => {
-    const topicMapItem = render(
-      <TopicMapItem backgroundImage={{ path: "", alt: "" }} title="Title" />,
-    ).container;
+    const item = {
+      id: "1",
+      topicImage: {
+        path: "https://images.unsplash.com/photo-1569587112025-0d460e81a126?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2370&q=80",
+        alt: "",
+      },
+      label: "Sheep in the distance",
+      description: "",
+      widthPercentage: 50,
+      heightPercentage: 25,
+      xPercentagePosition: 3,
+      yPercentagePosition: 5,
+      dialog: {
+        text: "Dialog text",
+      },
+    };
 
-    expect(topicMapItem.querySelector("button")).toBeTruthy();
+    const onClick = jest.fn();
+
+    const { container } = render(
+      <TopicMapItem item={item} onClick={onClick} />,
+    );
+
+    expect(container.querySelector("button")).toBeTruthy();
   });
 });
