5946dcee7266f0a67adb2df716b8855f296cde0b
Yijun <yijun.pan.stautland@tietoevry.com>
2022-02-04 21:15:43 +0100

    Update dialog lab with DialogContent instead

--- src/components/Dialog-Window/Tabs/DialogTabs.stories.tsx
+++ src/components/Dialog-Window/Tabs/DialogTabs.stories.tsx
@@ -14,10 +14,29 @@ const Template: ComponentStory<typeof DialogTabs> = args => (
 
 export const Tabs = Template.bind({});
 Tabs.args = {
-  tabContents: [
-    { title: "Text", content: <h1>Content for text goes here!</h1> },
-    { title: "Video", content: <h1>Content for video goes here!</h1> },
-    { title: "Resources", content: <h1>Content for resources goes here!</h1> },
-    { title: "Sound", content: <h1>Content for sound goes here!</h1> },
-  ],
+  tabContents: {
+    text: "Text text for testing Text text test.",
+    links: ["www.youtube.com", "www.ndla.com"],
+    video: {
+      path: "https://player.vimeo.com/video/224857514?title=0&portrait=0&byline=0&autoplay=1&loop=1&transparent=1",
+    },
+    audio: {
+      file: { path: "https://bigsoundbank.com/UPLOAD/mp3/0001.mp3" },
+      subtext: `
+        <b>
+          Den franske revolusjon var en periode med store sosiale og politiske
+          omveltningene i Frankrike i perioden 1789-1799. Året 1789 markerer det
+          første viktige vendepunktet under revolusjonen. 14. juli dette året brøt
+          det ut masseopprør i Paris og fengselet Bastillen ble stormet.
+        </b>
+        <br />
+        <p>
+          Perioden 1787-1789 kalles gjerne førrevolusjonen, men langsiktige
+          årsaker til revolusjonen strekker seg naturligvis mye lenger tilbake i
+          tid. De viktigste politiske, sosiale og administrative omveltninger
+          skjedde i perioden 1789-1794.
+        </p>
+    `,
+    },
+  },
 };
