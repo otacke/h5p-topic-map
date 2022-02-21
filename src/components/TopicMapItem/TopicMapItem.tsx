3743832d2f570ce68e139c8800b2a70582b85b89
Yijun <yijun.pan.stautland@tietoevry.com>
2022-02-21 13:29:29 +0100

    Add noteButton with localStorage hook

--- src/components/TopicMapItem/TopicMapItem.tsx
+++ src/components/TopicMapItem/TopicMapItem.tsx
@@ -2,7 +2,9 @@ import * as React from "react";
 import { FC, MouseEventHandler } from "react";
 import { useAppWidth } from "../../hooks/useAppWidth";
 import { BreakpointSize } from "../../types/BreakpointSize";
+import { useLocalStorage } from "../../hooks/useLocalStorage";
 import { TopicMapItemType } from "../../types/TopicMapItemType";
+import { NoteButton } from "../NoteButton/NoteButton";
 import styles from "./TopicMapItem.module.scss";
 
 export type TopicMapItemProps = {
@@ -23,6 +25,20 @@ export const TopicMapItem: FC<TopicMapItemProps> = ({ item, onClick }) => {
     () => [styles.topicMapItem, sizeClassname[appWidth]].join(" "),
     [appWidth],
   );
+  const [userData, setUserData] = useLocalStorage(item.id);
+  let btnState = 2;
+  if (item.dialog?.hasNote) {
+    switch (true) {
+      case userData[item.id].noteCompleted:
+        btnState = 0;
+        break;
+      case userData[item.id]?.note && userData[item.id]?.note?.length !== 0:
+        btnState = 1;
+        break;
+      default:
+        btnState = 2;
+    }
+  }
 
   return (
     <button type="button" className={className} onClick={onClick}>
@@ -41,6 +57,16 @@ export const TopicMapItem: FC<TopicMapItemProps> = ({ item, onClick }) => {
           item.topicImage?.path ? "" : styles.noImage
         }`}
       >
+        {item.dialog?.hasNote ? (
+          <div className={styles.button}>
+            <NoteButton
+              backgroundColor="var(--theme-color-3)"
+              borderColor="white"
+              iconColor="white"
+              buttonState={btnState}
+            />
+          </div>
+        ) : null}
         <div
           className={styles.label}
           // eslint-disable-next-line react/no-danger
