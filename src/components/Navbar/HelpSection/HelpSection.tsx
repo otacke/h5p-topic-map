11300be36b20c09c5596da74d3965b1d733bf84a
benq <beliy255@gmail.com>
2022-02-28 12:51:53 +0100

    replace media query in help section with useAppWidth breakpoints

--- src/components/Navbar/HelpSection/HelpSection.tsx
+++ src/components/Navbar/HelpSection/HelpSection.tsx
@@ -1,13 +1,29 @@
 import * as React from "react";
+import { useAppWidth } from "../../../hooks/useAppWidth";
 import { useL10n } from "../../../hooks/useLocalization";
+import { BreakpointSize } from "../../../types/BreakpointSize";
 import styles from "./HelpSection.module.scss";
+
+const sizeClassname = {
+  [BreakpointSize.Large]: styles.large,
+  [BreakpointSize.Medium]: styles.medium,
+  [BreakpointSize.Small]: styles.small,
+  [BreakpointSize.XSmall]: styles.xSmall,
+};
 
 export const HelpSection: React.FC = () => {
   const mainBodyTitle = useL10n("navbarHelpSectionTitle");
   const mainBodyText = useL10n("navbarHelpSectionBody");
 
+  const appWidth = useAppWidth();
+
+  const sizeClassName = React.useMemo(
+    () => sizeClassname[appWidth],
+    [appWidth],
+  );
+
   return (
-    <div className={styles.mainBody}>
+    <div className={`${styles.mainBody} ${sizeClassName}`}>
       <div className={styles.mainBodyContent}>
         <div className={styles.mainBodyTitle}>
           <p>{mainBodyTitle}</p>
